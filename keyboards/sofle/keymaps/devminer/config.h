39d9acd2dbbe2f7a316e5af4e7418187ad055c0a
DevMiner <devminer@devminer.xyz>
2025-09-03 02:33:10 +0200

    fix eeprom master

--- keyboards/sofle/keymaps/devminer/config.h
+++ keyboards/sofle/keymaps/devminer/config.h
@@ -5,7 +5,7 @@ see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
 
-#define MASTER_RIGHT
+#define EE_HANDS
 
 #define ENCODERS_PAD_A { F5 }
 #define ENCODERS_PAD_B { F4 }
